6a428fa986d647f208ec50ce314ae4fb0b9c9d13
carlos toledo <to1edo>
2022-10-06 00:56:43 -0400

    fix color bug

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -5,7 +5,7 @@ const Letter = ({letter, actual, color}) => {
 	const {darkMode} = useMain()
   return (
 		<>
-				<div className={`${ darkMode ? ' text-gray-200' : 'bg-gray-400'}  ${(darkMode && !color)? 'bg-black': ''} bg-${color}-500 ${ actual ? 'border-solid border-yellow-400 border-2' : ''} h-11 p-4 rounded-md  flex justify-center items-center uppercase text-4xl font-bold`}>{letter}</div>
+				<div className={`${ darkMode ? ' text-gray-200' : ''}  ${(darkMode && !color)? 'bg-black': ''} ${(!darkMode && !color)? 'bg-gray-400': ''} bg-${color}-500 ${ actual ? 'border-solid border-yellow-400 border-2' : ''} h-11 p-4 rounded-md  flex justify-center items-center uppercase text-4xl font-bold`}>{letter}</div>
 		</>
   )
 }
